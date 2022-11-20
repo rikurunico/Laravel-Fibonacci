f9fae82b6d63d2bee96e0d84872c657c5254b5d5
nicoco1173 <rikurunico@gmail.com>
2022-11-20 14:16:57 +0700

    Validation Flash

--- resources/views/HomePage/index.blade.php
+++ resources/views/HomePage/index.blade.php
@@ -14,7 +14,11 @@
            @if($errors->any())
             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                 <strong class="font-bold">Error!</strong>
-                <span class="block sm:inline">{{$errors->first()}}</span>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
             </div>
             @endif
 
